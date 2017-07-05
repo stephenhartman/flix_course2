38df16e1e10de9656898fbdb41479453ed9e55b6
stephenhartman <stephen.c.hartman@gmail.com>
2017-07-05 16:13:04 -0400

    Add update and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "User successfully updated!"
+    else
+      render :edit
+    end
+  end
+
   def new
     @user = User.new
   end
