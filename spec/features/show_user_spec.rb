6b5cf2764ff4d7b4d70249110e280d3aee7995ee
stephenhartman <stephen.c.hartman@gmail.com>
2017-07-05 12:10:39 -0400

    Add show action and view

--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Viewing a user's profile page" do
   it "shows the user's details" do
-    user = user.create!(user_attributes)
+    user = User.create!(user_attributes)
     visit user_url(user)
 
     expect(page).to have_text(user.name)
