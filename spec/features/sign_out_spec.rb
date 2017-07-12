9ee31d59b38d42b951ce4f526efea3f12531e2bb
stephenhartman <stephen.c.hartman@gmail.com>
2017-07-12 15:04:13 -0400

    Fixes and spec

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe 'Signing out' do
   it 'logs out the user' do
-    user = user.create!(user_attributes)
+    user = User.create!(user_attributes)
     sign_in(user)
 
     click_link('Sign Out')
 
-    expect(page).to have_text('Signed out')
+    expect(page).to have_text('signed out')
     expect(page).to_not have_link('Sign Out')
     expect(page).to have_link('Sign In')
   end
