0b71d6f63f26f424bb778cc218a4c1b2b6f5b39f
stephenhartman <stephen.c.hartman@gmail.com>
2017-07-17 14:44:18 -0400

    Add admin to users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705203139) do
+ActiveRecord::Schema.define(version: 20170717135103) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -40,9 +40,10 @@ ActiveRecord::Schema.define(version: 20170705203139) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "username"
+    t.boolean  "admin",           default: false
   end
 
 end
